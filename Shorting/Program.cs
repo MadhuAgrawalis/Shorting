c68971bbf8ab64d893b3b2db8514f15039baf75f
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-01-25 13:58:43 +0530

    Uc2

--- Shorting/Program.cs
+++ Shorting/Program.cs
@@ -1,142 +1,25 @@
 ﻿using System;
-
-namespace Shorting
-{
-    using System;
-
-
-    public class StackUsingLinkedlist
+using System.Collections.Generic;
+    namespace Shorting
     {
-
-
-        private class Node
-        {
-
-            public int data;
-
-
-            public Node link;
-        }
-
-
-        Node top;
-        public StackUsingLinkedlist()
-        {
-            this.top = null;
-        }
-
-
-        public void push(int x)
-        {
-
-            Node temp = new Node();
-
-
-            if (temp == null)
-            {
-                Console.Write("\nHeap Overflow");
-                return;
-            }
-
-
-            temp.data = x;
-
-
-            temp.link = top;
-
-
-            top = temp;
-        }
-
-
-        public bool isEmpty()
-        {
-            return top == null;
-        }
-
-
-        public int peek()
-        {
-
-            if (!isEmpty())
-            {
-                return top.data;
-            }
-            else
-            {
-                Console.WriteLine("Stack is empty");
-                return -1;
-            }
-        }
-
-
-        public void pop()
-        {
-
-            if (top == null)
-            {
-                Console.Write("\nStack Underflow");
-                return;
-            }
-
-
-            top = (top).link;
-        }
-
-        public void display()
-        {
-
-            if (top == null)
-            {
-                Console.Write("\nStack Underflow");
-                return;
-            }
-            else
-            {
-                Node temp = top;
-                while (temp != null)
-                {
-
-
-                    Console.Write("{0}->", temp.data);
-
-
-                    temp = temp.link;
-                }
-            }
-        }
-    }
-
-
-    public class GFG
+        public class Shorting
     {
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
+           var num = new Stack<int>();
+            num.Push(56);
+            num.Push(30);
+            num.Push(70);
 
-            StackUsingLinkedlist obj = new StackUsingLinkedlist();
+            foreach (int item in num)
+            {
+                Console.WriteLine(num);
+            }
 
+            Console.WriteLine("Peek element: " + num.Peek());
+            Console.WriteLine("Pop: " + num.Pop());
+            Console.WriteLine("After Pop, Peek element: " + num.Peek());
 
-            obj.push(70);
-            obj.push(30);
-            obj.push(56);
-
-
-
-            obj.display();
-
-
-            Console.Write("\nTop element is {0}\n", obj.peek());
-
-
-            obj.pop();
-            obj.pop();
-
-
-            obj.display();
-
-
-            Console.Write("\nTop element is {0}\n", obj.peek());
         }
     }
 }
- 
